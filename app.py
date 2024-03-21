93d54dcad3cf27e3dc0f54e7d1d6401773589009
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-03-21 16:22:37 +0530

    gffruyfr

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from test2 import callback1
 
 st.title("Person Counter App")
 webrtc_streamer(key="sample", video_frame_callback=callback1,media_stream_constraints={"video":True,"audio":False},
-                rtc_configuration={ "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+                rtc_configuration={ "iceServers": [{"urls": ["turn:openrelay.metered.ca:80"],"username":'openrelayproject',"credentials":"openrelayproject"}]})
 
 
