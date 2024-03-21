26836715097563b8b647465fa123e86498535a3b
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-03-21 18:49:31 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer,VideoHTMLAttributes
 from test2 import callback1
+import os
+
 
 
 st.title("Person Counter App")
 webrtc_streamer(key="sample", video_frame_callback=callback1,media_stream_constraints={"video":True,"audio":False},
-                rtc_configuration={ "iceServers": [{"urls": ["turn:global.relay.metered.ca:80"],"username":'51634103147da7f5e3a354b5',"credential":"P+HO7Cc0P3wIIMim"}]})
+                rtc_configuration={ "iceServers": [{"urls": [st.secrets["URL"]],"username":st.secrets["USERNAME"],"credential":st.secrets["CREDENTIAL"]}]})
 
 
+ 
