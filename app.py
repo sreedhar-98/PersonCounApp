7aa54ae8d61b626dd605c787710a80adb4f116b4
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-03-21 19:31:17 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer,VideoHTMLAttributes
 from test2 import callback1
-import os
+from twilio.rest import Client
 
+account_sid=st.secrets["sid"]
+auth_token=st.secrets["token"]
+client=Client(account_sid,auth_token)
 
+token=client.tokens.create()
 
 st.title("Person Counter App")
 webrtc_streamer(key="sample", video_frame_callback=callback1,media_stream_constraints={"video":True,"audio":False},
-                rtc_configuration={ "iceServers": [{"urls": [st.secrets["URL"]],"username":st.secrets["USERNAME"],"credential":st.secrets["CREDENTIAL"]}]})
+                rtc_configuration={ "iceServers": token.ice_servers}
+)
 
 
  
