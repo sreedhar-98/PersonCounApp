6a2228fdf61a969073beaf7db7caa8a60c53a6fd
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-03-21 16:28:21 +0530

    ddsadssd

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from test2 import callback1
 
 st.title("Person Counter App")
 webrtc_streamer(key="sample", video_frame_callback=callback1,media_stream_constraints={"video":True,"audio":False},
-                rtc_configuration={ "iceServers": [{"urls": ["turn:openrelay.metered.ca:80"],"username":'openrelayproject',"credential":"openrelayproject"}]})
+                rtc_configuration={ "iceServers": [{"urls": ["turn:openrelay.metered.ca:80"],"username":'51634103147da7f5e3a354b5',"credential":"P+HO7Cc0P3wIIMim"}]})
 
 
