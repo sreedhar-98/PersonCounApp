0b66536dc10a3080d4ea6f7938983529603f096e
sreedhar-98 <120257166+sreedhar-98@users.noreply.github.com>
2024-03-24 15:28:05 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,16 +9,16 @@ def callback1(frame):
     model_weights = 'MobileNetSSD_deploy.caffemodel'
     net = cv2.dnn.readNetFromCaffe(model_prototxt, model_weights)
 
-    # start_time = time.time()  
+    start_time = time.time()  
 
     frame, face_count = detect_faces_mobilenetssd(frame.copy(), net)
 
     cv2.putText(frame, f"Live Face Count: {face_count}", (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
 
-    # end_time = time.time()
-    # fps = 1 / (end_time - start_time)
-    # cv2.putText(frame, f"FPS: {fps:.1f}", (10, 60),
-    #             cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+    end_time = time.time()
+    fps = 1 / (end_time - start_time)
+    cv2.putText(frame, f"FPS: {fps:.1f}", (10, 60),
+                 cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
     
     return av.VideoFrame.from_ndarray(frame,format="bgr24")
